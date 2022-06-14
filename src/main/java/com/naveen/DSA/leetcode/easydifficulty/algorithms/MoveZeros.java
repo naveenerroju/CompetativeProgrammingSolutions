999e2e4f1c92adfc92e3a3f9e0d6d83c4de1e988
Naveen Kumar <naveenkumarerroju@gmail.com>
2022-06-14 17:24:29 +0530

    MoveZeros.java in leetcode O(n) solution added.

--- src/main/java/com/naveen/DSA/leetcode/easydifficulty/algorithms/MoveZeros.java
+++ src/main/java/com/naveen/DSA/leetcode/easydifficulty/algorithms/MoveZeros.java
@@ -12,13 +12,46 @@ import java.util.Arrays;
 
 public class MoveZeros {
 
-    public static int[] solution(int[] nums){
+    public static int[] solution(int[] nums){       // O(n)
 
+        int insertPosition = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=0){
+                nums[insertPosition] = nums[i];
+                insertPosition++;
+            }
+        }
+
+        while (insertPosition<nums.length){
+            nums[insertPosition]=0;
+            insertPosition++;
+        }
 
         return nums;
     }
 
-    public static int[] bruteForce(int[] nums){
+
+    //Although this is a simple solution, leetcode made it sure that it wont accept solutions that are using different arrays.
+    //we must use the same array they provided. because we are not returning anything inside the leetcode editor.
+    //returning is only for my convenience for easy testing.
+    // THIS IS NOT A SPACE EFFICIENT METHOD. BECAUSE WE ARE CREATING ANOTHER COPY OF ARRAY.
+    public static int[] alternateApproach(int[] nums){
+
+        int[] copy = new int[nums.length];
+        int copyInd = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=0){
+                copy[copyInd] = nums[i];
+                copyInd++;
+            }
+        }
+
+        return copy;
+    }
+
+    public static int[] bruteForce(int[] nums){         // O(n^2)
 
         if(nums.length<=1)
             return nums;
