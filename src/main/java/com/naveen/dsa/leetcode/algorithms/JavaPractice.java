55869bae874b74ff61c9795b47d277be72f2c179
PraneethaCh <praneetha2022@gmail.com>
2023-07-12 16:09:16 +0530

    questions are given

--- src/main/java/com/naveen/dsa/leetcode/algorithms/JavaPractice.java
+++ src/main/java/com/naveen/dsa/leetcode/algorithms/JavaPractice.java
@@ -9,8 +9,25 @@ public class JavaPractice {
 
     }
 
-    public static void practice(){
+    /**
+     * Given an array of integers, sort them in ascending order and return the new sorted array.
+     * Test cases will contain negative integers and zeros.
+     * @param nums
+     */
+   public static int[] sortingArrayInteger(int[] nums){
 
-        
+       return null;
+   }
+
+    /**
+     * Given the array of strings with various lengths, sort them in the ascending order from shortest string to the longest string
+     * Ex: {"a","cccccc","bbbb","xx"} => {"a","xx","bbbb","cccccc"}
+     *
+     * @return
+     */
+   public static String[] sortedArrayString(){
+
+       return null;
     }
+
 }
