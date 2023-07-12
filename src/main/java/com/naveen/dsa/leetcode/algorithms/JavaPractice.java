7aa4f8d442585cb15ef5b6d5769d35b1b67524a7
Praneetha <chandolu.praneetha@capgemini.com>
2023-07-12 15:05:35 +0530

    initial commit

---
+++ src/main/java/com/naveen/dsa/leetcode/algorithms/JavaPractice.java
@@ -0,0 +1,16 @@
+package com.naveen.dsa.leetcode.algorithms;
+
+/**
+ * @author Praneetha
+ * @Description this is a trash class please ignore
+ */
+public class JavaPractice {
+    public static void main(String[] args) {
+
+    }
+
+    public static void practice(){
+
+        
+    }
+}
