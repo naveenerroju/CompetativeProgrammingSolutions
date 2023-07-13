a41b57b08564ea3f962938d081801c9cae87b571
PraneethaCh <praneetha2022@gmail.com>
2023-07-13 14:39:42 +0530

    Sorted and executed in DO.

--- src/main/java/com/naveen/dsa/leetcode/algorithms/JavaPractice.java
+++ src/main/java/com/naveen/dsa/leetcode/algorithms/JavaPractice.java
@@ -1,33 +1,41 @@
 package com.naveen.dsa.leetcode.algorithms;
 
+import java.awt.*;
+import java.net.StandardSocketOptions;
+
 /**
  * @author Praneetha
  * @Description this is a trash class please ignore
  */
 public class JavaPractice {
     public static void main(String[] args) {
+        int[] arr = {10,12,0,-2,-4,5};
+        int[] newArr = sortingArrayInteger(arr);
+        for (int num : newArr)
+        {
+            System.out.println(num);
+        }
 
     }
 
     /**
      * Given an array of integers, sort them in ascending order and return the new sorted array.
      * Test cases will contain negative integers and zeros.
+     *
      * @param nums
      */
-   public static int[] sortingArrayInteger(int[] nums){
-
-       return null;
-   }
-
-    /**
-     * Given the array of strings with various lengths, sort them in the ascending order from shortest string to the longest string
-     * Ex: {"a","cccccc","bbbb","xx"} => {"a","xx","bbbb","cccccc"}
-     *
-     * @return
-     */
-   public static String[] sortedArrayString(){
-
-       return null;
+    public static int[] sortingArrayInteger(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (nums[j-1] < nums[j]) {
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
     }
-
 }
+
