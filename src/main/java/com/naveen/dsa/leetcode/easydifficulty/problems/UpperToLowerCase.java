bac0590dcef5d461a31dfbc94a65dd3784b8da7f
PraneethaCh <praneetha2022@gmail.com>
2023-07-21 11:14:22 +0530

    Solved UpperToLowerCase

---
+++ src/main/java/com/naveen/dsa/leetcode/easydifficulty/problems/UpperToLowerCase.java
@@ -0,0 +1,15 @@
+package com.naveen.dsa.leetcode.easydifficulty.problems;
+
+public class UpperToLowerCase
+{
+    public static void main(String[] args)
+    {
+        String s = "ITS Raining";
+        String s1 = s.toLowerCase();
+        System.out.println(s1);
+    }
+    public String toLowerCase(String s)
+    {
+        return s;
+    }
+}
