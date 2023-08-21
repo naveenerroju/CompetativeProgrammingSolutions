142930759698f068d096672953db9006de410b51
naveen kumar <naveenkumarerroju@gmail.com>
2023-08-21 13:38:27 +0530

    MergeTwoSortedLists.java is added, CommonUtility.linkedListToString and CommonUtility.printLinkedLists methods added, Main.java is added

--- src/main/java/com/naveen/utils/CommonUtility.java
+++ src/main/java/com/naveen/utils/CommonUtility.java
@@ -3,7 +3,7 @@ package com.naveen.utils;
 import java.util.List;
 
 /**
- *This class contains basic functionality/util functions
+ * This class contains basic functionality/util functions
  * As a good coding practice, it is important to implement only utility functions and make them final static
  *
  * @author Naveen Kumar
@@ -12,6 +12,7 @@ public class CommonUtility {
 
     /**
      * This method is used to get a String representation of a list
+     *
      * @param list
      * @return
      */
@@ -26,6 +27,7 @@ public class CommonUtility {
 
     /**
      * This method is used to get a String representation of a list of integers
+     *
      * @param list
      * @return
      */
@@ -36,5 +38,33 @@ public class CommonUtility {
             buffer.append(",");
         }
         return buffer.substring(0, buffer.length() - 1);
+    }
+
+    /**
+     * This method is used to print a LinkedList (com.naveen.utils)
+     * @param node
+     */
+    public static final void printLinkedLists(ListNode node) {
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    /**
+     * This method is used to get a String representation of a LinkedList (com.naveen.utils)
+     * @param node
+     * @return
+     */
+    public static final String linkedListToString(ListNode node) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("[");
+
+        while (node != null) {
+            sb.append(node.val + ",");
+            node = node.next;
+        }
+
+        return sb.substring(0, sb.length() - 1) + "]";
     }
 }
